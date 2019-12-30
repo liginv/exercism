7043a6b21d2cc731b2bc00388444dd1216ef5d10
Ligin Vellakkad <ligindd@gmail.com>
2019-12-30 17:17:04 +0530

    Add solution to resistor-color-duo.

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,10 +1,45 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
+class ResistorColorDuo
+  @@black = "0"
+  @@brown = "1"
+  @@red = "2"
+  @@orange = "3"
+  @@yellow = "4" 
+  @@green = "5"
+  @@blue = "6"
+  @@violet = "7"
+  @@grey = "8"
+  @@white = "9"
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
+  def self.value(list)
+    first = list[0]
+    second = list[1]
+    res1 = number(first)
+    res2 = number(second)
+    result = res1+res2
+    result.to_i
+  end
 
-
-
+  def self.number(color)
+    if color.downcase == 'black'
+      @@black
+    elsif color.downcase == 'brown'
+      @@brown
+    elsif color.downcase == 'red'
+      @@red
+    elsif color.downcase == 'orange'
+      @@orange
+    elsif color.downcase == 'yellow'
+      @@yellow
+    elsif color.downcase == 'green'
+      @@green
+    elsif color.downcase == 'blue'
+      @@blue
+    elsif color.downcase == 'violet'
+      @@violet
+    elsif color.downcase == 'grey'
+      @@grey
+    elsif color.downcase == 'white'
+      @@white
+    end
+  end
+end
